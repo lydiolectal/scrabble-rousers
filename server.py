bd5dd12b553585a441c75befef05172b222605cd
lydiolectal <lydiding@gmail.com>
2018-05-29 16:32:50 -0400

    Write route functions to play game and update board display

--- server.py
+++ server.py
@@ -1,11 +1,12 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template, redirect
 from src.game import Game
 
 app = Flask(__name__)
+game = Game()
 
-@app.route('/')
-def root():
-    return app.send_static_file('index.html')
+# @app.route('/')
+# def root():
+#     return app.send_static_file('index.html')
 
 @app.route('/start', methods = ["GET"])
 def start_game():
@@ -20,18 +21,17 @@ def start_game():
                  }
     return jsonify(game_state)
 
-@app.route('/next-move', methods = ["GET"])
+@app.route('/', methods = ["GET"])
+def root():
+
+    # TODO: law of Demeter (bug Jordan)
+    return render_template("index.html", board = game.board.tiles,
+        tiles = game.cur_player.tiles, score = game.cur_player.recent_score)
+
+@app.route('/', methods = ["POST"])
 def next_move():
     game.play_one_move()
-    game_state = {
-                    "state": game.board.tiles,
-                    "player1_tiles": game.player1.tiles,
-                    "player2_tiles": game.player2.tiles,
-                 }
-    return jsonify(game_state)
-
-# @app.route('/end-game', methods = ["GET"])
-# def end_game():
-#     return 'Hello, World!'
-#     game = Game()
-#     game.play_one_move()
+    # redirect sends back a response code 302 ("I'm sending you to another location")
+    # one of the headers has "location: /" -- the browser then knows to follow
+    # it by sending a GET request to that URL.
+    return redirect("/")
